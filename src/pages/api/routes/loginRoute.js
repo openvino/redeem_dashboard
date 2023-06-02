fbb593e5b7d53b3530ce8666ab21543f1947a15a
Nachito02 <arguellojuan08@gmail.com>
2023-06-01 21:44:37 -0300

    add all domains for images in next config, authcontroller, getProfile function

--- src/pages/api/routes/loginRoute.js
+++ src/pages/api/routes/loginRoute.js
@@ -1,28 +1,19 @@
-import { verify } from "jsonwebtoken";
-import { serialize } from "cookie";
 import { NextResponse } from "next/server";
+import conn from "../config/db";
+import { getProfile } from "../controllers/authContoller";
 export default async function handler(req, res) {
 
 
-  try {
-    const token = verify(
-      req.body.token,
-      process.env.NEXT_PUBLIC_JWT_SECRET
-    );
-      if(token) {
-        const serialized = serialize('token',req.body.token, {
-          httpOnly:true,
-          secure: process.env.NODE_ENV === 'production',
-          sameSite: 'strict',
-          maxAge: 1000 * 60 * 60 *24 *30,
-          path: '/'
-        })
-            res.setHeader('Set-Cookie',serialized)
-            res.redirect('/dashboard')
-            res.status(200).json('Login sucess')
-
+      if(req.method == 'POST') {
+        try {
+          const {public_key} = req.body
+          const profile = await getProfile(public_key)
+          if(profile) {
+           res.status(200).json(profile)
+          }
+        } catch (error) {
+          res.status(400).json(error.message)
+        }
       }
-  } catch (error) {
-    res.status(404).json(error);
-  }
+
 }
