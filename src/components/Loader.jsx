87cd97c840c0753a4657f759477b9b4f2698ff36
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-06-14 14:01:13 -0300

    loader styles

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,15 +3,14 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="text-center">
+    <div className="flex justify-center items-center h-screen w-screen bg-[#F1EDE2] ">
+      <div className="text-center rounded-full w-[150px] h-[150px] ">
         <Image
           src={"/assets/loading.gif"}
-          className="m-auto"
+          className="m-auto rounded-full"
           width={200}
           height={200}
         />
-        <h1>Loading...</h1>
       </div>
     </div>
   );
