3dac75742b0100e80b6d57d1687b006f20c78098
Pablo Levy <114173733+pabloele@users.noreply.github.com>
2023-06-15 16:18:41 -0300

    removed language imports

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,31 +1,29 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
 
 // Archivo de traducción
-import translationES from '../../locales/es/translationES.json';
-import translationEN from '../../locales/en/translationEN.json';
+// import translationES from '../../locales/es/translationES.json';
+// import translationEN from '../../locales/en/translationEN.json';
 
-i18n
-  .use(initReactI18next)
-  .init({
-    // Idiomas admitidos
-    lng: 'es',
-    fallbackLng: 'es',
+i18n.use(initReactI18next).init({
+  // Idiomas admitidos
+  lng: "es",
+  fallbackLng: "es",
 
-    // Archivos de traducción
-    resources: {
-      es: {
-        translation: translationES,
-      },
-      en: {
-        translation: translationEN,
-      },
+  // Archivos de traducción
+  resources: {
+    es: {
+      translation: "",
     },
-
-    // Opciones de configuración adicionales
-    interpolation: {
-      escapeValue: false, // No escapar valores HTML en las traducciones
+    en: {
+      translation: "",
     },
-  });
+  },
+
+  // Opciones de configuración adicionales
+  interpolation: {
+    escapeValue: false, // No escapar valores HTML en las traducciones
+  },
+});
 
 export default i18n;
