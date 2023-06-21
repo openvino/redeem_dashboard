25e605c6720426d0b50daa3d1785be75877f9bbf
Juan Arguello <84349906+Nachito02@users.noreply.github.com>
2023-06-21 01:15:30 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const nextConfig = {
             value: 'http://147.182.205.152', // Cambia esto al origen específico permitido
           },
           {
+            key: 'Access-Control-Allow-Headers',
+            value: '*', // Cambia esto al origen específico permitido
+          },
+          {
             key: 'Access-Control-Allow-Methods',
             value: 'GET, PUT, POST, DELETE', // Permitir los métodos GET, PUT, POST y DELETE
           },
