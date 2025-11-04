d1a02d06015455d261dfeab78497d8aca8e01902
Pablo Levy <pabloelevy@gmail.com>
2025-11-04 09:36:06 -0300

    redesign topbar layout

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -182,64 +182,44 @@ const Topbar = () => {
 						</div>
 					</div>
 
-					<div className="w-calc-full-6rem md:w-full bg-[#F1EDE2]  bg-opacity-70 md:shadow-xl flex flex-col md:flex-row justify-evenly gap-2 border p-4 rounded-lg items-center flex-1">
-						<div className="flex justify-evenly items-center w-full gap-1 ">
-							<div>
-								<SidebarMobile />
-							</div>
-							{/* {router.pathname === "/dashboard" && (
-								<div className="relative inline-block">
-									<FormField
-										type="text"
-										className={`w-full rounded-lg border-none pl-10 focus:outline-[#925d78]`}
-										placeholder={t("buscar")}
-										onFocus={() => setIsFocused(true)}
-										onBlur={() => setIsFocused(false)}
-										ref={inputRef}
-										onChange={handleFilter}
-									/>
-									<span
-										id="search-icon"
-										className={`absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer ${
-											isFocused ? "opacity-0" : "opacity-100"
-										}`}
-										onClick={() => inputRef.current?.focus()}
-									>
-										<FaSearch className="hidden md:block" />
-									</span>
-								</div>
-							)} */}
+					<div className="px-8 md:px-20 w-calc-full-6rem md:w-full bg-[#F1EDE2]  bg-opacity-70 md:shadow-xl flex flex-col gap-1 border p-2 md:p-4 rounded-lg items-center flex-1 md:flex-row md:items-center md:justify-between">
+						<div className="flex w-full items-center justify-between gap-3 md:w-auto md:justify-start">
+							<SidebarMobile />
 
-							<ConnectButton
-								client={client}
-								locale="es_ES"
-								connectModal={{
-									size: "compact",
-									title: "Viniswap",
-									welcomeScreen: {
-										title: "Connect Wallet",
-										img: "/mtb.png",
-									},
-								}}
-								connectButton={{
-									label: "Connect Wallet",
-									style: {
-										padding: "12px 24px",
-										background: "#000",
-										color: "#fff",
-										fontSize: "16px",
-										fontWeight: "bold",
-										borderRadius: "12px",
-										boxShadow:
-											"0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08)",
-									},
-								}}
-							/>
+							<div className="flex items-center">
+								<ConnectButton
+									client={client}
+									locale="es_ES"
+									connectModal={{
+										size: "compact",
+										title: "Viniswap",
+										welcomeScreen: {
+											title: "Connect Wallet",
+											img: "/mtb.png",
+										},
+									}}
+									connectButton={{
+										label: "Connect Wallet",
+										style: {
+											padding: "12px 24px",
+											background: "#000",
+											color: "#fff",
+											fontSize: "16px",
+											fontWeight: "bold",
+											borderRadius: "12px",
+											boxShadow:
+												"0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08)",
+										},
+									}}
+								/>
+							</div>
+						</div>
+						<div className="flex items-center justify-end w-full gap-1 md:w-auto md:gap-1">
 							{allNotifications.length ? (
 								<div
 									className={
 										notification.notification
-											? " cursor-pointer my-4 p-3 rounded-full inline-block text-[#840C4A] pr-4 hover:transform hover:scale-105 transition-all duration-500"
+											? "cursor-pointer p-3 rounded-full inline-block text-[#840C4A] hover:transform hover:scale-105 transition-all duration-500"
 											: "hidden"
 									}
 									onClick={handleModal}
@@ -251,7 +231,7 @@ const Topbar = () => {
 							) : null}
 							<div
 								onClick={() => setShowMenu(!showMenu)}
-								className="relative shadow-xl  rounded-full hover:transform hover:scale-110 transition-all duration-500"
+								className="relative shadow-xl rounded-full hover:transform hover:scale-110 transition-all duration-500"
 							>
 								{profile?.profile_img && (
 									<Image
@@ -264,15 +244,15 @@ const Topbar = () => {
 								)}
 
 								{/* {showMenu && (
-										<div className="absolute w-[112px] cursor-pointer right-[-25px] top-15 bg-[#F1EDE2] border rounded-lg shadow-lg text-center text-sm ">
-											<p
-												className="m-0 p-2 cursor-pointer"
-												onClick={async () => await signOut({ redirect: "/" })}
-											>
-												Cerrar Sesión
-											</p>
-										</div>
-									)} */}
+									<div className="absolute w-[112px] cursor-pointer right-[-25px] top-15 bg-[#F1EDE2] border rounded-lg shadow-lg text-center text-sm ">
+										<p
+											className="m-0 p-2 cursor-pointer"
+											onClick={async () => await signOut({ redirect: "/" })}
+										>
+											Cerrar Sesión
+										</p>
+									</div>
+								)} */}
 							</div>
 							<button onClick={toggleLanguage}>
 								{selectLanguage ? "EN" : "ES"}
